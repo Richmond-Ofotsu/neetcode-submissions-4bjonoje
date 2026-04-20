278eac7b6c8f835287a2ece1185c427c86a44da0
Richmond Ofotsu <99608729+Richmond-Ofotsu@users.noreply.github.com>
2026-04-20 11:08:09 +0000

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cs
@@ -0,0 +1,35 @@
+public class Solution 
+{
+    public bool hasDuplicate(int[] nums) 
+    {
+        // HashSet<int> set = new HashSet<int>();    
+
+        // foreach (int num in nums)  
+        // {
+        //     if (!set.Contains(num))
+        //     {
+        //         set.Add(num);
+        //     }
+        //     else 
+        //     {
+        //         return true;
+        //     }
+        // }  
+
+        // return false;
+
+        HashSet<int> set = new();
+        foreach (int num in nums)
+        {
+            if (set.Contains(num))
+            {
+                return true;
+            }
+            else 
+            {
+                set.Add(num);
+            }    
+        }
+        return false;   
+    }
+}
